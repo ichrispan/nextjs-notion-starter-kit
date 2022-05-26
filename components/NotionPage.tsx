d951ca1bc84ff0505d635cf1aa83ce3912f47eb6
Priders_zyh <672116147@qq.com>
2022-05-27 01:05:53 +0800

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -270,12 +270,12 @@ export const NotionPage: React.FC<types.PageProps> = ({
         searchNotion={searchNotion}
         pageFooter={comments}
         pageAside={pageAside}
-        footer={
+        /*footer={
           <Footer
             isDarkMode={darkMode.value}
             toggleDarkMode={darkMode.toggle}
           />
-        }
+        }*/
       />
 
       <GitHubShareButton />
